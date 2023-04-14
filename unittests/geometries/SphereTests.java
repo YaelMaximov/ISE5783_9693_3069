43eec6a4830e58ef6c491e3c1acbfd0f955ea2bf
Moria123 <morimiz42@gmail.com>
2023-04-14 16:46:45 +0300

    1127

--- unittests/geometries/SphereTests.java
+++ unittests/geometries/SphereTests.java
@@ -12,14 +12,11 @@ class SphereTests {
      * Test method for {@link geometries.Sphere#getNormal(primitives.Point)}.
      */
     @Test
-    // ============ Equivalence Partitions Tests ==============
-    //TC01:There is a simple single test here
     void testGetNormal() throws IllegalAccessException {
+        // ============ Equivalence Partitions Tests ==============
+        //TC01:There is a simple single test here
         Sphere sphere = new Sphere(1.0, new Point(1, 0, 0));
         Vector vector = new Vector(0, 1, 1);
         assertEquals(vector, sphere.getNormal(new Point(1, 1, 1)), "Bad normal to sphere");
-
-
-
     }
 }
---
+++ unittests/geometries/TriangleTests.java
@@ -0,0 +1,22 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTests {
+    /**
+     * Test method for {@link geometries.Triangle#getNormal(primitives.Point)}.
+     */
+    @Test
+    void testGetNormal() throws IllegalAccessException {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: There is a simple single test here
+        Triangle triangle = new Triangle(new Point(1, 0, 0), new Point(1, 1, 1), new Point(0, 0, 0));
+        Vector vector = new Vector(0.0D, -0.7071067811865475D, 0.7071067811865475D);
+        assertEquals(vector, triangle.getNormal(new Point(0, 0, 0)), "Bad normal to triangle");
+    }
+
+}
