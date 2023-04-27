531ebd4d2ed796ac2f80d51590def77bd072e80a
yaeli <yaelimaximov@gmail.com>
2023-04-27 16:32:43 +0300

    Ex3

---
+++ unittests/IntegrationTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+import renderer.Camera;
+
+public class IntegrationTests {
+    @Test
+    void CameraSphereIntersections() {
+        Camera camera=new Camera(new Point());
+
+    }
+    @Test
+    void CameraPlaneIntersections() {
+    }
+    @Test
+    void CameraTriangleIntersections() {
+    }
+}
