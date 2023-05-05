86be22bc5c3b1fffdf9d358c8a89c51821207bce
Moria123 <morimiz42@gmail.com>
2023-05-05 15:39:32 +0300

    PRO 5-

--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -7,25 +7,50 @@ import scene.Scene;
 
 import java.util.List;
 
+/**
+ * A basic implementation of a RayTracer.
+ */
 public class RayTracerBasic extends RayTracerBase {
 
-
+    /**
+     * Constructs a new RayTracerBasic object with the given scene.
+     *
+     * @param scene the scene to be rendered
+     */
     public RayTracerBasic(Scene scene) {
         super(scene);
     }
 
+    /**
+     * Computes the color of the given ray in the scene and returns it.
+     *
+     * @param ray the ray to be traced
+     * @return the color of the ray in the scene
+     * @throws IllegalAccessException if there is an error accessing the scene or objects in the scene
+     */
     @Override
     public Color traceRay(Ray ray) throws IllegalAccessException {
+        // Find intersections of the ray with objects in the scene
         List<Point> list = scene.geometries.findIntsersections(ray);
         if (list == null) {
+            // If there are no intersections, return the background color of the scene
             return scene.background;
         }
-        Point closesPoint = ray.findClosestPoint(list);
-        return calcColor(closesPoint);
+        // Find the closest intersection point
+        Point closestPoint = ray.findClosestPoint(list);
+        // Calculate the color of the point using the scene's ambient light
+        return calcColor(closestPoint);
     }
 
+    /**
+     * Calculates the color of the given point using the scene's ambient light.
+     *
+     * @param point the point to calculate the color for
+     * @return the color of the point using the scene's ambient light
+     */
     private Color calcColor(Point point){
         return scene.ambientLight.getIntensity();
     }
 
 }
+
