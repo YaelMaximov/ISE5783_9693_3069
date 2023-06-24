1d1c0baeb255680101af40541155a66ac156c4e2
Moria123 <morimiz42@gmail.com>
2023-06-24 23:21:41 +0300

    a

--- unittests/pictures/Pictures.java
+++ unittests/pictures/Pictures.java
@@ -1,7 +1,9 @@
 package pictures;
 
+import geometries.Cylinder;
 import geometries.Geometry;
 import geometries.Plane;
+import geometries.Sphere;
 import lighting.DirectionalLight;
 import lighting.PointLight;
 import lighting.SpotLight;
@@ -11,6 +13,8 @@ import renderer.Camera;
 import renderer.ImageWriter;
 import renderer.RayTracerBasic;
 import scene.Scene;
+
+import static java.awt.Color.BLUE;
 
 public class Pictures {
 
@@ -42,6 +46,11 @@ public class Pictures {
             .setEmission(new Color(17,45,81)).setMaterial(material);
     private final Geometry sky3 =new Plane(new Point(3000, 50,-150), new Point(3000, 250, -3000), new Point(3000,250, -60))
             .setEmission(new Color(17,45,81)).setMaterial(material);
+    private final Geometry streetLampC = new Cylinder(new Ray(new Point(45,-35,-20),new Vector(0,1,0)),1,30).setEmission(new Color(255,255,255));
+    private final Geometry moon1 = new Sphere(20, new Point(80,230,-2998)).setEmission(new Color(255,255,255));
+    private final Geometry moon2 = new Sphere(20, new Point(90,230,-2998)).setEmission(new Color(17,45,81));
+    private final Geometry s = new Sphere(5d, new Point(45,0,-20)).setEmission(new Color(255,255,255)) //
+            .setMaterial(new Material().setkD(0.5).setkS(0.5).setnShininess(30));
 
     //new Color(143,211,240)
     private final Geometry floor =new Plane(new Point(1500, -100, -1500), new Point(-1500, -100, -1500),
@@ -70,6 +79,15 @@ public class Pictures {
                 .setkL(0.0001).setkQ(0.0002));
         scene.lights.add(new PointLight(new Color(800, 255, 255), new Point(-230, 200, -2998))
                 .setkL(0.0001).setkQ(0.0002));
+        scene.lights.add(new PointLight(new Color(800, 255, 255), new Point(-235, 200, -2998))
+                .setkL(0.0001).setkQ(0.0002));
+        //scene.lights.add(new PointLight(new Color(800, 255, 255),new Point(45,0,-20))
+          //      .setkL(0.0001).setkQ(0.0002));
+        //scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(45,0,-20), new Vector(0, 0, -1)) //
+          //      .setkL(4E-5).setkQ(2E-7));
+        scene.lights.add( //
+                new PointLight(new Color(255,255,255), new Point(40, 0,-20)) //
+                        .setkL(0.01).setkQ(2E-5));
 
     }
     public void  setupGeometries(Scene scene) throws IllegalAccessException
@@ -90,7 +108,7 @@ public class Pictures {
         Pyramid p=new Pyramid(p2,z+z*0.2,x+x*0.2,30d).setPyramidEmission(new Color(126,147,229)).setPyramidMaterial(material);
         Clock clock=new Clock(p3,l).setClockEmission(new Color(255,255,255),new Color(218,178,86)).setClockMaterial(material);
 
-        scene1.geometries.add(clock.getGeometries(),p.getGeometries(),base.getGeometries(),building.getGeometries(),sky1,sky2,sky3,floor);
+        scene1.geometries.add(clock.getGeometries(),p.getGeometries(),base.getGeometries(),building.getGeometries(),sky1,sky2,floor,s,streetLampC,moon1,moon2);
     }
 
 
